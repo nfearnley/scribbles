7126520554588850f9193021acb35a236256aeff
Natalie Fearnley <nfearnley@gmail.com>
2019-12-09 15:30:23 -0500

    Added logger

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use strict";
 
+const log = require("loglevel");
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const config = require("./config");
+
+client.once("ready", function() {
+    log.info("Ready!");
+});
+
+client.login(config.discord.token);
